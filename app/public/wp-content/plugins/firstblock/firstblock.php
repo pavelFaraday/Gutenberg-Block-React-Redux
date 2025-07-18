3bd123a46aa4b57bde5900720b481c03c0edbe95
“pavelFaraday” <devstudio.corp@gmail.com>
2025-07-18 13:10:17 +0400

    Udemy - Gutenberg Blocks & Plugins React/Redux
    - Create 'First Block' Plugin

---
+++ app/public/wp-content/plugins/firstblock/firstblock.php
@@ -0,0 +1,9 @@
+<?php
+
+/**
+ * Plugin Name: First Block
+ * Plugin URI: https://www.linkedin.com/in/giorgi-epitashvili/
+ * Description: My first block
+ * Author: Giorgi Epitashvili
+ * Author URI: https://www.linkedin.com/in/giorgi-epitashvili/
+ */
