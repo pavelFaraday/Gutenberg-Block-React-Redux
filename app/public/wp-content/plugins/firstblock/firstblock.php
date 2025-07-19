1e790594de130d1cee6c5189f014c7054066efb7
“pavelFaraday” <devstudio.corp@gmail.com>
2025-07-19 08:13:27 +0400

    Udemy - Gutenberg Blocks & Plugins React/Redux
    - Registering our First Block

--- app/public/wp-content/plugins/firstblock/firstblock.php
+++ app/public/wp-content/plugins/firstblock/firstblock.php
@@ -7,3 +7,10 @@
  * Author: Giorgi Epitashvili
  * Author URI: https://www.linkedin.com/in/giorgi-epitashvili/
  */
+
+function blocks_course_firstblock_init()
+{
+    // Registers a block type from the metadata stored in the block.json file.
+    register_block_type_from_metadata(__DIR__);
+}
+add_action("init", "blocks_course_firstblock_init");
