44b6aded9c17f6a49c0d6c28d8ba88c12682bdb9
“pavelFaraday” <devstudio.corp@gmail.com>
2025-07-21 13:35:55 +0400

    Udemy - Gutenberg Blocks & Plugins React/Redux
    - Install Initial Package

---
+++ app/public/wp-content/plugins/firstblock/build/blocks-manifest.php
@@ -0,0 +1,25 @@
+<?php
+// This file is generated. Do not modify it manually.
+return array(
+	'firstblock' => array(
+		'$schema' => 'https://schemas.wp.org/trunk/block.json',
+		'apiVersion' => 3,
+		'name' => 'create-block/firstblock',
+		'version' => '0.1.0',
+		'title' => 'Firstblock',
+		'category' => 'text',
+		'icon' => 'format-status',
+		'description' => 'My first block',
+		'example' => array(
+			
+		),
+		'supports' => array(
+			'html' => false
+		),
+		'textdomain' => 'firstblock',
+		'editorScript' => 'file:./index.js',
+		'editorStyle' => 'file:./index.css',
+		'style' => 'file:./style-index.css',
+		'viewScript' => 'file:./view.js'
+	)
+);
---
+++ app/public/wp-content/plugins/firstblock/build/firstblock/view.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array(), 'version' => '9fbba897b6d1c8cd0ad6');
