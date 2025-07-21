9ef65faf09d459b97b4076dfafc7ab19e9e681fd
“pavelFaraday” <devstudio.corp@gmail.com>
2025-07-21 13:55:05 +0400

    Udemy - Gutenberg Blocks & Plugins React/Redux
    - Compiling & Loading SASS Files

--- app/public/wp-content/plugins/firstblock/build/firstblock/index.asset.php
+++ app/public/wp-content/plugins/firstblock/build/firstblock/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => 'e8bdd030a71f0d302887');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => '215d830051b207920908');
