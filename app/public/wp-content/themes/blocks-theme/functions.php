5dc0ceeb4303bbc13594f0577bd3021dbcaf4111
“pavelFaraday” <devstudio.corp@gmail.com>
2025-07-22 14:59:44 +0400

    Udemy - Gutenberg Blocks & Plugins React/Redux
    - Enhance theme setup by adding support for wide alignment and removing deprecated comments; add multiple image sizes for better responsiveness.

--- app/public/wp-content/themes/blocks-theme/functions.php
+++ app/public/wp-content/themes/blocks-theme/functions.php
@@ -29,30 +29,15 @@ function blocks_theme_setup()
 	add_theme_support(
 		'responsive-embeds'
 	);
-	/*
-		* Make theme available for translation.
-		* Translations can be filed in the /languages/ directory.
-		* If you're building a theme based on Blocks Theme, use a find and replace
-		* to change 'blocks-theme' to the name of your theme in all the template files.
-		*/
+	add_theme_support(
+		'align-wide'
+	);
+
 	load_theme_textdomain('blocks-theme', get_template_directory() . '/languages');
 
 	// Add default posts and comments RSS feed links to head.
 	add_theme_support('automatic-feed-links');
-
-	/*
-		* Let WordPress manage the document title.
-		* By adding theme support, we declare that this theme does not use a
-		* hard-coded <title> tag in the document head, and expect WordPress to
-		* provide it for us.
-		*/
 	add_theme_support('title-tag');
-
-	/*
-		* Enable support for Post Thumbnails on posts and pages.
-		*
-		* @link https://developer.wordpress.org/themes/functionality/featured-images-post-thumbnails/
-		*/
 	add_theme_support('post-thumbnails');
 
 	// This theme uses wp_nav_menu() in one location.
@@ -62,10 +47,6 @@ function blocks_theme_setup()
 		)
 	);
 
-	/*
-		* Switch default core markup for search form, comment form, and comments
-		* to output valid HTML5.
-		*/
 	add_theme_support(
 		'html5',
 		array(
@@ -94,11 +75,6 @@ function blocks_theme_setup()
 	// Add theme support for selective refresh for widgets.
 	add_theme_support('customize-selective-refresh-widgets');
 
-	/**
-	 * Add support for core custom logo.
-	 *
-	 * @link https://codex.wordpress.org/Theme_Logo
-	 */
 	add_theme_support(
 		'custom-logo',
 		array(
