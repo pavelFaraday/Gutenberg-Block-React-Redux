b6de1457f5cc832e9493e33426d109f540c53fbf
“pavelFaraday” <devstudio.corp@gmail.com>
2025-07-23 12:10:14 +0400

    Udemy - Gutenberg Blocks & Plugins React/Redux
    - Create 'Text Box' block plugin with initial setup and functionality

---
+++ app/public/wp-content/plugins/text-box/build/blocks-manifest.php
@@ -0,0 +1,31 @@
+<?php
+// This file is generated. Do not modify it manually.
+return array(
+	'text-box' => array(
+		'$schema' => 'https://schemas.wp.org/trunk/block.json',
+		'apiVersion' => 3,
+		'name' => 'create-block/text-box',
+		'version' => '0.1.0',
+		'title' => 'Text Box',
+		'category' => 'text',
+		'icon' => 'text-page',
+		'description' => 'A simple text box.',
+		'example' => array(
+			
+		),
+		'keywords' => array(
+			'text',
+			'box',
+			'content',
+			'paragraph'
+		),
+		'supports' => array(
+			'html' => false
+		),
+		'textdomain' => 'text-box',
+		'editorScript' => 'file:./index.js',
+		'editorStyle' => 'file:./index.css',
+		'style' => 'file:./style-index.css',
+		'viewScript' => 'file:./view.js'
+	)
+);
---
+++ app/public/wp-content/plugins/text-box/build/text-box/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => 'c33381d9c1580e6b6665');
