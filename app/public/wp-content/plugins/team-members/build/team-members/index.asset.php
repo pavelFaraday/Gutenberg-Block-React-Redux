5a00df16cc6e6a2187f5af4fa9820c38ec93d3ed
“pavelFaraday” <devstudio.corp@gmail.com>
2025-07-30 13:07:16 +0400

    Enhance Team Members Block
    - Updated editor styles to add margin to media placeholders and team member images for improved layout.
    - Modified save function to include image URL, alt text, and ID attributes for team members, allowing for dynamic image rendering.

--- app/public/wp-content/plugins/team-members/build/team-members/index.asset.php
+++ app/public/wp-content/plugins/team-members/build/team-members/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-blob', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => '3895fc1652f836c8503b');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-blob', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => '0027cc3451d2e2370af5');
--- app/public/wp-content/plugins/team-members/src/team-members/team-member/save.js
+++ app/public/wp-content/plugins/team-members/src/team-members/team-member/save.js
@@ -1,9 +1,14 @@
 import { useBlockProps, RichText } from "@wordpress/block-editor";
 
 export default function save({ attributes }) {
-	const { name, bio } = attributes;
+	const { name, bio, url, alt, id } = attributes;
 	return (
 		<div {...useBlockProps.save()}>
+			{url && (
+				<div className="wp-block-blocks-course-team-member-img">
+					<img src={url} alt={alt} className={id ? `wp-image-${id}` : null} />
+				</div>
+			)}
 			<RichText.Content tagName="h4" value={name} />
 			<RichText.Content tagName="p" value={bio} />
 		</div>
